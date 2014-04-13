ee495e19bbefa0938a810737079ff5e641b272a6
Chris Schmich <schmch@gmail.com>
2014-04-13 15:27:48 -0700

    Updating tests to use mocha.

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var jisonify = require('../index');
 var assert = require('assert');
 var vm = require('vm');
 
-function expectValue(fileName, b, value) {
+function expectValue(fileName, b, value, done) {
   b.add(__dirname + '/' + fileName);
   b.transform(jisonify);
 
@@ -22,19 +22,31 @@ function expectValue(fileName, b, value) {
     vm.runInNewContext(src, sandbox);
 
     assert.equal(sandbox.test.result, value);
+
+    done();
   });
 }
 
-function expectError(fileName, b) {
-  b.add(__dirname + '/' + fileName);
-  b.transform(jisonify);
-
-  b.bundle(function(err, src) {
-    if (!err)
-      assert.fail('Expected error parsing Jison file.');
+describe('jisonify', function () {
+  it('processes a script with jison', function (done) {
+    expectValue('s1.js', browserify(), 42, done);
   });
-}
 
-expectValue('s1.js', browserify(), 42);
-expectValue('s2.js', browserify({ extensions: '.jison' }), 42);
-expectError('s3.js', browserify());
+  it('processes a script with jison with implicit extension', function (done) {
+    expectValue('s2.js', browserify({ extensions: '.jison' }), 42, done);
+  });
+
+  it('reports errors', function (done) {
+    var fileName = 's3.js';
+    var b = browserify();
+    b.add(__dirname + '/' + fileName);
+    b.transform(jisonify);
+
+    b.bundle(function(err, src) {
+      if (!err)
+        assert.fail('Expected error parsing Jison file.');
+
+      done();
+    });
+  });
+});
